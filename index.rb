831cbfa1bffd9407064ab56c76a9b82b016d69a3
blakes <blake.j.symington@gmail.com>
2019-09-04 10:25:17 +1000

    Added close_quiz/ run quiz methods to quiz_screen method

--- index.rb
+++ index.rb
@@ -1,10 +1,30 @@
 
-	def welcome
+	require 'json'
+  require 'colorize'
+  def welcome
       puts "Hello, welcome to Trivia Quiz."
   end
-    def quiz_screen()
+    def quiz_screen
       puts "Are you ready to play? -.- (Y/N)"
       input = gets.chomp
+      cmd = input.upcase
+       case cmd
+       when 'Y'
+        run_quiz
+       when 'N'
+        close_quiz
+       else
+        puts "Invailid Input! Please try again."
+        quiz_screen
+       end
+    end
+    def run_quiz
+      puts "Running Trivia Quiz. You are ready to go!"
+    end
+
+    def close_quiz
+      puts "Aborted Trivia Quiz."
+      abort
     end
     welcome
     quiz_screen
