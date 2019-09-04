c4d5189605c818db5bcea0b1d907d148ff639107
blakes <blake.j.symington@gmail.com>
2019-09-04 11:04:19 +1000

    Refined index.rb added colorize text to each methods

--- README.md
+++ README.md
@@ -3,3 +3,25 @@
 **Trivia Quiz in the CMD** is my first CLI written in ruby and is to be assessed as our first ruby assignment for the [Coder  Academy](https://coderacademy.edu.au/) FastTrack Bootcamp.
 
 Its a simple, intriguing, challenging application **(Trivia Quiz)** that display true or false questions for the user to answer with either 1 or 0 answer which also calculate their score at the end of the quiz
+
+## Clone
+
+- Clone  this  repo  to  your  local  machine  using  `https://github.com/blakes/ca_terminal_app.git`
+
+## Features
+
+
+- Verify they want to play the quiz 
+
+- load json file of questions in allocation
+
+- User can answer true or false for each question their score is calculated at the end of the quiz
+
+## Help  File
+​
+Dependencies used are:
+
+​
+
+- colorize - Ruby string class extension. It add some methods to set 
+- json - Implementation of the JSON specification to read or write json files
--- index.rb
+++ index.rb
@@ -1,11 +1,12 @@
 
 	require 'json'
   require 'colorize'
+  
   def welcome
       puts "Hello, welcome to Trivia Quiz."
   end
     def quiz_screen
-      puts "Are you ready to play? -.- (Y/N)"
+      puts "Are you ready to play? -.- (Y/N)\n".colorize(:blue)
       input = gets.chomp
       cmd = input.upcase
        case cmd
@@ -14,17 +15,52 @@
        when 'N'
         close_quiz
        else
-        puts "Invailid Input! Please try again."
+        puts "Invailid Input! Please try again.\n".colorize(:red)
         quiz_screen
        end
     end
+
     def run_quiz
-      puts "Running Trivia Quiz. You are ready to go!"
+      puts "Running Trivia Quiz. You are ready to go!\n".colorize(:green)
+      file = load_file
+      load_questions(file.sample(10))
+    end
+
+    def load_file
+    f = File.open("trivia.json","r")
+    return JSON.parse(f.read())
+    f.close()
     end
 
     def close_quiz
-      puts "Aborted Trivia Quiz."
+      puts "Aborted Trivia Quiz.".colorize(:red)
       abort
     end
+
+    def load_questions(arrs)
+      puts "Please type 1 for true answer or type 0 for false answer. type x to exit\n"
+          puts "\n"
+      answer = ""
+      score = 0
+     for arr in arrs
+          puts arr['question'].colorize(:light_blue)
+          answer = gets.chomp()
+          if answer == arr['answer_bool']
+               score += 1
+               puts "Correct Answer! \n".colorize(:green)
+               puts arr['answer_body'].colorize(:black)
+               puts "\n"
+             elsif answer != arr['answer_bool']
+              puts "Wrong Answer!".colorize(:red)
+               puts arr['answer_body'].colorize(:black)
+               puts "\n"
+            else 
+              puts "Invailid Reponse".colorize(:red)
+          end
+          end
+          puts "you got #{score} out of #{arr.length()} \n"
+          puts "End of Trivia Quiz. Thanks for Playing"
+          quiz_screen
+     end
     welcome
     quiz_screen
