dd2fc77500482603f9ae0678c2c97d2094762ecd
blakes <blake.j.symington@gmail.com>
2019-09-04 10:14:42 +1000

    Added welcome and quiz_screen method

--- index.rb
+++ index.rb
@@ -1,2 +1,10 @@
 
-	
+	def welcome
+      puts "Hello, welcome to Trivia Quiz."
+  end
+    def quiz_screen()
+      puts "Are you ready to play? -.- (Y/N)"
+      input = gets.chomp
+    end
+    welcome
+    quiz_screen
